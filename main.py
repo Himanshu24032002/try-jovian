01f60c40a3e22ab29fbb4229cd0ed22374a04818
Himanshu Bora <116859666+Himanshu24032002@users.noreply.github.com>
2022-11-01 17:30:02 +0000

    creating html

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 #flask = module and Flask = class
 #route = part of url after the domain name
 @app.route("/")
 def hello_himanshu():
- return "Hello, Himanshu"
+   return render_template('home.html')
 
-if __name__== "__main__":
+if __name__== '__main__':
  app.run(host='0.0.0.0', debug=True) 
